b5bc7d207690a6fa5fa560a5a4ac16f19cf799d7
Jonathan Lipps <jlipps@gmail.com>
2014-11-11 13:17:14 -0800

    initial logic

---
+++ test/es6/specs.js
@@ -0,0 +1,20 @@
+/* global describe:true, it:true */
+
+import 'traceur/bin/traceur-runtime';
+import 'mochawait';
+import should from 'should';
+import path from 'path';
+import { getModified } from '../../lib/es5/main';
+console.log(getModified);
+
+describe("getModified", () => {
+  describe("with a real repo", () => {
+    it("should return something", async () => {
+      let repo = path.resolve(__dirname, "..", "..");
+      let res = await getModified(repo);
+      should.exist(res);
+      should.ok(res instanceof Array);
+    });
+  });
+});
+
